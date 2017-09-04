6a950abd4334d6a6654cc8e2a88bb5f476fe0433
Louis <louisaleksieb.dagusen@gmail.com>
2017-09-04 15:08:39 +0800

    1 commit

---
+++ myapp/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Post
+#from .forms import PostForm
+#login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+# Create your views here.
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'post_detail.html', {'post': post})
+
+#Accounts
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('post_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
